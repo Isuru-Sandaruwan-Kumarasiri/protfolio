eeb1db08bacfa769b40525acb2cdda4e5b11161d
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-01-04 21:02:09 +0530

    created responsive home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Header from "./components/header/Header"
+import Home from './components/home/Home'
 
 function App() {
  
@@ -8,6 +9,9 @@ function App() {
   return (
     <>
       <Header/>
+      <main className="main">
+        <Home/>
+      </main>
       
     </>
   )
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Social() {
+  return (
+    <div className="home__social">
+        <a href="" className="home__social-icon">
+            <i class="uil uil-facebook"></i>
+        </a>
+        <a href="" className="home__social-icon">
+            <i class="uil uil-linkedin"></i>
+        </a>
+        <a href="https://github.com/Isuru-Sandaruwan-Kumarasiri" className="home__social-icon">
+           <i class="uil uil-github"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
